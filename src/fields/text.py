143285329f8ec50b6a96829789134661209ad8e6
ihateb335 <zhar.misha@gmail.com>
2026-05-28 13:31:03 +0300

    Add text

---
+++ src/fields/text.py
@@ -0,0 +1,17 @@
+from src.fields.field import Field
+
+
+class Text(Field):
+    """Клас для зберігання тексту.
+
+    Аргументи:
+        Field: Базовий клас для всіх полів.
+    """
+
+    def validate(self):
+        """Перевіряє текст.
+
+        Повертає:
+            bool: True, якщо текст валідний, інакше False.
+        """
+        return isinstance(self.value, str) and self.value.strip()
