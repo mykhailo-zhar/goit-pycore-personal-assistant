38f5d284900faf6af293ec0ff4428127d1fd122c
ihateb335 <zhar.misha@gmail.com>
2026-05-28 13:30:58 +0300

    Refactor tag

--- src/fields/tag.py
+++ src/fields/tag.py
@@ -1,19 +1,24 @@
 import re
+
 from src.fields.field import Field
 
 
 class Tag(Field):
-  """Клас для зберігання тегів контакту.
+    """Клас для зберігання тегів контакту.
 
-  Аргументи:
-        Field: Базовий клас для всіх полів.
-  """    
-  TAG_PATTERN = re.compile(r"^[a-z0-9]{1,30}$")
-
-  def validate(self):
-    """Перевіряє теги.
-
-    Повертає:
-        bool: True, якщо теги валідні, інакше False.
+    Аргументи:
+          Field: Базовий клас для всіх полів.
     """
-    return re.match(self.TAG_PATTERN, self.value) is not None
+
+    TAG_PATTERN = re.compile(r"^[a-z0-9]{1,30}$")
+
+    def validate(self):
+        """Перевіряє теги.
+
+        Повертає:
+            bool: True, якщо теги валідні, інакше False.
+        """
+        return (
+            isinstance(self.value, str)
+            and re.match(self.TAG_PATTERN, self.value) is not None
+        )
