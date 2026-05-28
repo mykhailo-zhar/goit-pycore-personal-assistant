caccdc124229285c5a02c696fe33c3a23b54221d
ihateb335 <zhar.misha@gmail.com>
2026-05-28 14:13:42 +0300

    Add remove_contact command

---
+++ src/commands/remove_contact.py
@@ -0,0 +1,58 @@
+from src.address_book import AddressBook
+from src.scripts.contacts_bot import input_error
+
+REMOVE_CONTACT_MESSAGES = {
+    "CONTACT_NOT_FOUND": "Contact {name} not found",
+    "CONTACT_REMOVED": "Contact {name} removed",
+    "INVALID_SYNTAX": "Remove contact command should have the following syntax: remove <name> [phone]",
+    "PHONE_NOT_FOUND": "Phone {phone} not found for contact {name}",
+    "PHONE_REMOVED": "Phone removed",
+}
+
+
+def try_remove_contact(book: AddressBook, name: str) -> str:
+    """
+    Видаляє телефон з запису.
+
+    Аргументи:
+        book: Адресна книга.
+        name: Ім'я контакту.
+
+    Винятки:
+        KeyError: Якщо контакт не знайдено.
+
+    Повертає:
+        str: Відповідь на команду.
+    """
+    if not book.remove_record(name):
+        raise KeyError(REMOVE_CONTACT_MESSAGES["CONTACT_NOT_FOUND"].format(name=name))
+    return REMOVE_CONTACT_MESSAGES["CONTACT_REMOVED"].format(name=name)
+
+
+@input_error
+def remove_contact(book: AddressBook, arguments: list[str]) -> str:
+    """
+    Видаляє контакт з адресної книги.
+
+    Аргументи:
+        book (AddressBook): Адресна книга.
+        arguments (list[str]): Ім'я контакту.
+    """
+    if 1 < len(arguments) > 2:
+        raise ValueError(REMOVE_CONTACT_MESSAGES["INVALID_SYNTAX"])
+    name = arguments[0]
+    record = book.find_record(name)
+    if not record:
+        raise KeyError(REMOVE_CONTACT_MESSAGES["CONTACT_NOT_FOUND"].format(name=name))
+
+    if len(arguments) == 2:
+        phone = arguments[1]
+    else:
+        return try_remove_contact(book, name)
+
+    record.remove_phone(phone)
+    if not record.find_phone(phone):
+        raise KeyError(
+            REMOVE_CONTACT_MESSAGES["PHONE_NOT_FOUND"].format(phone=phone, name=name)
+        )
+    return REMOVE_CONTACT_MESSAGES["PHONE_REMOVED"]
