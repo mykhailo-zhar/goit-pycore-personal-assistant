2225e43fffc63c1dab75ef63af2cb760307c9de1
ihateb335 <zhar.misha@gmail.com>
2026-05-28 12:05:38 +0300

    Add email

---
+++ src/fields/email.py
@@ -0,0 +1,18 @@
+import re
+from src.fields.field import Field
+
+
+class Email(Field):
+  """Клас для Email з валідацією формату.
+
+  Аргументи:
+        Field: Базовий клас для всіх полів.
+  """    
+
+  def validate(self):
+    """Перевіряє email.
+
+    Повертає:
+        bool: True, якщо email валідний, інакше False.
+    """
+    return re.match(r"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", self.value) is not None
