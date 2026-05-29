e39f863ac0754cf7324f7647d2a2b53a2801b5c6
barkode <konstantinks@gmail.com>
2026-05-29 23:20:25 +0100

    Refactor remove_note command messages for improved clarity and error handling

--- src/commands/remove_note.py
+++ src/commands/remove_note.py
@@ -2,10 +2,10 @@ from src.note import Note
 from src.note_book import NoteBook
 from src.utils.decorators.input_error import input_error
 
-ADD_NOTE_MESSAGES = {
-    "INVALID_SYNTAX": "Add note command should have the following syntax: add-note <title>",
-    "NOTE_ALREADY_PRESENT": "Note is already present.",
-    "NOTE_ADDED": "Note added: {title}",
+REMOVE_NOTE_MESSAGES = {
+    "INVALID_SYNTAX": "Remove note command should have the following syntax: remove-note <title>",
+    "NOTE_ALREADY_DELETED": "Note is already deleted.",
+    "NOTE_NOT_FOUND": "Note not found.",
     }
 
 
@@ -21,11 +21,13 @@ def remove_note(note_book: NoteBook, arguments: list[str]) -> bool:
             bool: True, якщо нотатку знайдено і видалено, інакше False.
         """
     if len(arguments) != 1:
-        raise ValueError(ADD_NOTE_MESSAGES["INVALID_SYNTAX"])
+        raise ValueError(REMOVE_NOTE_MESSAGES["INVALID_SYNTAX"])
     title = arguments[0]
 
     if note_book.find_note(title):
         del note_book.data[title]
+        print(REMOVE_NOTE_MESSAGES["NOTE_ALREADY_DELETED"])
         return True
 
+    print(REMOVE_NOTE_MESSAGES["NOTE_NOT_FOUND"])
     return False
