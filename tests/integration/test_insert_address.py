ef328020039cb58adcb3518f214d5b11b92c4650
ihateb335 <zhar.misha@gmail.com>
2026-05-29 16:21:45 +0300

    Fix insert address

--- tests/integration/test_insert_address.py
+++ tests/integration/test_insert_address.py
@@ -4,7 +4,7 @@ import pytest
 
 from src.commands.insert_address import INSERT_ADDRESS_MESSAGES
 from src.scripts.contacts_bot import main
-from src.utils.address_book_serializer import AddressBookSerializer
+from src.utils.serializers.address_book import AddressBookSerializer
 
 
 @pytest.fixture
