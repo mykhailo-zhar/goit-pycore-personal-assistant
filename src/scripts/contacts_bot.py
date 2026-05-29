871e36372e4973c4ee395a0a5a752e853bc6559c
barkode <konstantinks@gmail.com>
2026-05-29 23:44:29 +0100

    Refactor: move command messages to a separate file

--- src/scripts/contacts_bot.py
+++ src/scripts/contacts_bot.py
@@ -25,28 +25,7 @@ if __name__ == "__main__":
 
 from src.address_book import AddressBook
 from src.note_book import NoteBook
-
-COMMAND_MESSAGES = {
-    "INVALID_COMMAND": "Invalid command.",
-    "CONTACT_ADDED": "Contact added.",
-    "CONTACT_UPDATED": "Contact updated.",
-    "BIRTHDAY_ADDED": "Birthday added. Replacing {old_birthday} with {new_birthday} for {name}",
-    "NO_BIRTHDAY_SET": "No birthday set for {name}",
-    "BIRTHDAY_SHOWED": "Birthday for {name} is {birthday}",
-    "UPCOMING_BIRTHDAYS": "Upcoming birthdays:\n{birthdays}",
-    "NO_SUCH_USER": "No such user",
-    "PLEASE_CHANGE_USER": "Please change the user",
-    "GOOD_BYE": "Good bye!",
-    "HELLO": "How can I help you?",
-    "PHONE_CHANGED": "Phone was changed",
-    "PHONE_CHANGE_SYNTAX": "Syntax: change-phone <name> <old phone> <new phone>",
-    "BIRTHDAYS_SYNTAX": "Syntax: birthdays <days>",
-    "BIRTHDAYS_DAYS": "Days must be a non-negative integer.",
-    "BIRTHDAYS_NO_UPCOMMING": "No upcoming birthdays.",
-    "BIRTHDAYS_FORMAT": "%d.%m.%Y",
-    "PHONES_TRUNCATED": "Phones updated for {name}: {phone}",
-    "TRUNCATE_SYNTAX": "Syntax: truncate <name> <new_phone>",
-}
+from src.commands.messages import COMMAND_MESSAGES
 
 SERIALIZER_PATH = "addressbook.pkl"
 NOTE_SERIALIZER_PATH = "notebook.pkl"
