e53dd866313132d2df00674ab587a591e3cf778a
barkode <konstantinks@gmail.com>
2026-05-29 23:14:40 +0100

    Implement remove_note function to delete notes by title

--- src/commands/remove_note.py
+++ src/commands/remove_note.py
@@ -1,2 +1,31 @@
-def remove_note(title: str, note_book: NoteBook) -> bool:
-    pass
+from src.note import Note
+from src.note_book import NoteBook
+from src.utils.decorators.input_error import input_error
+
+ADD_NOTE_MESSAGES = {
+    "INVALID_SYNTAX": "Add note command should have the following syntax: add-note <title>",
+    "NOTE_ALREADY_PRESENT": "Note is already present.",
+    "NOTE_ADDED": "Note added: {title}",
+    }
+
+
+@input_error
+def remove_note(note_book: NoteBook, arguments: list[str]) -> bool:
+    """
+        Видаляє нотатку за заголовком.
+
+        Аргументи:
+            title (str): Заголовок нотатки для видалення.
+
+        Повертає:
+            bool: True, якщо нотатку знайдено і видалено, інакше False.
+        """
+    if len(arguments) != 1:
+        raise ValueError(ADD_NOTE_MESSAGES["INVALID_SYNTAX"])
+    title = arguments[0]
+
+    if note_book.find_note(title):
+        del note_book.data[title]
+        return True
+
+    return False
