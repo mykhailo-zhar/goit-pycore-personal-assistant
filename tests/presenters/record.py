38c2cb8c19e23f541720586f965e307cc210ba7a
ihateb335 <zhar.misha@gmail.com>
2026-05-30 14:49:52 +0300

    Rich table is not compatible with current layout

--- tests/presenters/record.py
+++
@@ -1,9 +0,0 @@
-from src.record import Record
-
-
-class RecordPresenter:
-    def __init__(self, record: Record):
-        self.record = record
-
-    def __str__(self) -> str:
-        return str(self.record)
