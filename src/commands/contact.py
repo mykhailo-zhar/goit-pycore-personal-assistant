87ba5275e5d17c72991ccd8d67abab0f0430a51a
ihateb335 <zhar.misha@gmail.com>
2026-05-30 11:09:02 +0300

    Add contact

---
+++ src/commands/contact.py
@@ -0,0 +1,27 @@
+from src.address_book import AddressBook
+from src.utils.decorators.input_error import input_error
+
+CONTACT_MESSAGES = {
+    "INVALID_SYNTAX": "Contact command should have the following syntax: contact <name>",
+    "NO_SUCH_CONTACT": "No such contact.",
+}
+
+
+@input_error
+def contact(book: AddressBook, arguments: list[str]) -> str:
+    """
+    Показує інформацію про контакт за іменем.
+
+    Аргументи:
+        book (AddressBook): Адресна книга.
+        arguments (list[str]): Ім'я контакту.
+    """
+    if len(arguments) != 1:
+        raise ValueError(CONTACT_MESSAGES["INVALID_SYNTAX"])
+
+    name = arguments[0]
+    record = book.find_record(name)
+    if record is None:
+        raise ValueError(CONTACT_MESSAGES["NO_SUCH_CONTACT"])
+
+    return str(record)
