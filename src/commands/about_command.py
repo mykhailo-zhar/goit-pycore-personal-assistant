77bc28105441cc13e7cafc6822818f67d4f6d19a
barkode <konstantinks@gmail.com>
2026-05-30 13:16:33 +0100

    Feature/remove-personal-links-from-about-command

--- src/commands/about_command.py
+++ src/commands/about_command.py
@@ -4,32 +4,22 @@ from src.address_book import AddressBook
 TEAM_MEMBERS = [
     {
         "name": "Mykhailo Zhar",
-        "github": "https://github.com/username",
-        "linkedin": "https://linkedin.com/in/username",
         "email": "mykhailo.zhar@stud.onu.edu.ua",
         },
     {
         "name": "Kostiantyn Krysenko",
-        "github": "https://github.com/barkode",
-        "linkedin": "https://www.linkedin.com/in/kostiantyn-krysenko/",
         "email": "konstantinks@gmail.com",
         },
     {
         "name": "Olga Pushkar",
-        "github": "https://github.com/username",
-        "linkedin": "https://linkedin.com/in/username",
         "email": "gglolga@gmail.com",
         },
     {
         "name": "Mykhailo Kovalchuk",
-        "github": "https://github.com/username",
-        "linkedin": "https://linkedin.com/in/username",
         "email": "mykhailokovalchuck@gmail.com",
         },
     {
         "name": "Gleb Kislovskyi",
-        "github": "https://github.com/username",
-        "linkedin": "https://linkedin.com/in/username",
         "email": "gkislovskyi@gmail.com",
         },
     ]
@@ -50,8 +40,6 @@ def about_command(_: AddressBook, arguments: list[str]) -> str:
 
     for member in TEAM_MEMBERS:
         lines.append(f"  {member['name']}")
-        lines.append(f"    GitHub:   {member['github']}")
-        lines.append(f"    LinkedIn: {member['linkedin']}")
         lines.append(f"    Email: {member['email']}")
         lines.append("")
 
